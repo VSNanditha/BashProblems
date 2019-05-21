f0a7bf7c88149bfa17a607e91453e82cf29aa235
Syamala Nanditha Vittanala <v.shyamalananditha@gmail.com>
2019-05-20 20:52:46 -0500

    Adding tail command problems

---
+++ tail1.sh
@@ -0,0 +1,10 @@
+# In this challenge, we practice using the tail command to display the last n
+# lines of a text file. Display the last 20 lines of an input file.
+#
+# Input Format
+# A text file.
+#
+# Constraints
+# Output the last 20 lines of the text file.
+
+tail -n 20 $1
