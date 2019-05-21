c07be487d66430c317983ad09f6a334eb600ce4a
Syamala Nanditha Vittanala <v.shyamalananditha@gmail.com>
2019-05-20 20:52:17 -0500

    Adding head command problems

---
+++ head1.sh
@@ -0,0 +1,10 @@
+# In this challenge, we practice using the head command to display the first
+# n lines of a text file. Display the first 20 lines of an input file.
+#
+# Input Format
+# A text file.
+#
+# Output Format
+# Output the first 20 lines of the given text file.
+
+head -20 $1
